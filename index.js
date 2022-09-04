29c49f5901095220ef781e3c515c412959b5a5b2
Romuald Brillout <git@brillout.com>
2022-09-04 18:12:10 +0200

    rename `@brillout/json-s` -> `@brillout/json-serializer`

--- index.js
+++
@@ -1,1 +0,0 @@
-throw new Error("Module `@brillout/json-s` doesn't exist anymore: load `@brillout/json-s/parse` and `@brillout/json-s/stringify` instead.")
--- src/utils/isReactElement.ts
+++
@@ -1,7 +0,0 @@
-export function isReactElement(value: unknown) {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    String((value as Record<string, unknown>)['$$typeof']) === 'Symbol(react.element)'
-  )
-}
